b1fcf29acbf2b9bbfd468311cf46abb43e0bfc64
Egor Borisevich <picassonk21@gmail.com>
2021-05-27 21:13:36 +0300

    feat: add news slider

--- pages/news/js/news.js
+++ pages/news/js/news.js
@@ -19,11 +19,11 @@ const sliderItems = document.querySelectorAll('.slider__item');
 sliderDots.forEach((item) => {
   item.addEventListener('click', () => {
     const position = [...sliderDots].indexOf(item) + 1;
-    const difference = position - activeSlide;
+    const step = position - activeSlide;
     if (position > activeSlide) {
-      slideRight(difference);
+      slideRight(step);
     } else if (position < activeSlide) {
-      slideLeft(difference);
+      slideLeft(step);
     }
   });
 });
@@ -36,8 +36,8 @@ sliderControlsRight.addEventListener('click', () => {
   slideRight(1);
 });
 
-function slideLeft(difference) {
-  activeSlide += difference;
+function slideLeft(step) {
+  activeSlide += step;
   slideDots();
   if (activeSlide === 1) {
     toggleLeftDisabled(true);
@@ -46,12 +46,11 @@ function slideLeft(difference) {
     toggleLeftDisabled(false);
     toggleRightDisabled(false);
   }
-  moveSlide(true, difference);
-  console.log(activeSlide);
+  moveSlide(step);
 };
 
-function slideRight(difference) {
-  activeSlide += difference;
+function slideRight(step) {
+  activeSlide += step;
   slideDots();
   if (activeSlide === 4) {
     toggleLeftDisabled(false);
@@ -60,8 +59,7 @@ function slideRight(difference) {
     toggleRightDisabled(false);
     toggleLeftDisabled(false);
   }
-  moveSlide(false, difference);
-  console.log(activeSlide);
+  moveSlide(step);
 };
 
 function toggleRightDisabled(isDisabled) {
@@ -85,10 +83,94 @@ function slideDots() {
   sliderDots[activeSlide - 1].classList.add('active');
 };
 
-function moveSlide(isLeft = true, difference) {
+function moveSlide(step) {
   sliderItems.forEach((item) => {
     const currentPos = +item.getAttribute('data-pos');
-    const newPos = currentPos + difference;
+    const newPos = currentPos + step;
+    item.setAttribute('data-pos', newPos)
+  });
+};
+
+let currentPage = 1;
+const newsSliderList = document.querySelectorAll('.news__slider-item');
+const newsSliderPages = document.querySelectorAll('.news__slider-controls__item');
+const newsSliderControlsLeft = document.querySelector('.news__slider-controls-left');
+const newsSliderControlsLeftImage = document.querySelector('.news__slider-controls-left img');
+const newsSliderControlsRight = document.querySelector('.news__slider-controls-right');
+const newsSliderControlsRightImage = document.querySelector('.news__slider-controls-right img');
+
+newsSliderPages.forEach((item) => {
+  item.addEventListener('click', () => {
+    const position = [...newsSliderPages].indexOf(item) + 1;
+    const step = position - currentPage;
+    if (position > currentPage) {
+      newsSlideRight(step);
+    } else if (position < currentPage) {
+      newsSlideLeft(step);
+    }
+  });
+});
+
+newsSliderControlsLeft.addEventListener('click', () => {
+  newsSlideLeft(-1);
+});
+
+newsSliderControlsRight.addEventListener('click', () => {
+  newsSlideRight(1);
+});
+
+function toggleNewsControlsLeftDisabled(isDisabled) {
+  newsSliderControlsLeft.classList.toggle('disabled', isDisabled);
+  newsSliderControlsLeft.classList.contains('disabled')
+    ? newsSliderControlsLeftImage.src = '../../assets/arrow-left.svg'
+    : newsSliderControlsLeftImage.src = '../../assets/arrow-left-active.svg';
+};
+
+function toggleNewsControlsRightDisabled(isDisabled) {
+  
+  newsSliderControlsRight.classList.toggle('disabled', isDisabled);
+  newsSliderControlsRight.classList.contains('disabled')
+    ? newsSliderControlsRightImage.src = '../../assets/arrow-right.svg'
+    : newsSliderControlsRightImage.src = '../../assets/arrow-right-active.svg';
+};
+
+function newsSlideLeft(step) {
+  currentPage += step;
+  changeCurrentPage();
+  if (currentPage === 1) {
+    toggleNewsControlsLeftDisabled(true);
+    toggleNewsControlsRightDisabled(false);
+  } else {
+    toggleNewsControlsLeftDisabled(false);
+    toggleNewsControlsRightDisabled(false);
+  }
+  moveNews(step * 2);
+};
+
+function newsSlideRight(step) {
+  currentPage += step;
+  changeCurrentPage();
+  if (currentPage === 4) {
+    toggleNewsControlsLeftDisabled(false);
+    toggleNewsControlsRightDisabled(true);
+  } else {
+    toggleNewsControlsRightDisabled(false);
+    toggleNewsControlsLeftDisabled(false);
+  }
+  moveNews(step * 2);
+};
+
+function changeCurrentPage() {
+  newsSliderPages.forEach((item) => {
+    item.classList.remove('active');
+  });
+  newsSliderPages[currentPage - 1].classList.add('active');
+};
+
+function moveNews(step) {
+  newsSliderList.forEach((item) => {
+    const currentPos = +item.getAttribute('data-pos');
+    const newPos = currentPos + step;
     item.setAttribute('data-pos', newPos)
   });
 };
