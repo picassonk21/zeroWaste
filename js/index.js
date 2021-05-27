e3b702a5e5bf2054cd718b73032e761a763e9fd0
Egor Borisevich <picassonk21@gmail.com>
2021-05-27 22:33:00 +0300

    feat: add slider logic on index.html page

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const aboutMenuItemList = document.querySelectorAll('.about__menu-item');
 const aboutImage = document.querySelector('.about__image img');
 
 aboutMenuItemList.forEach((item) => {
-  item.addEventListener('click', () => {
+  item.addEventListener('click', (e) => {
     aboutMenuItemList.forEach((item) => {
       item.classList.remove('active');
     });
@@ -11,3 +11,29 @@ aboutMenuItemList.forEach((item) => {
     aboutImage.src = `./assets/about${pos}.jpg`;
   });
 });
+
+const sliderList = document.querySelectorAll('.intro__slider-item');
+const sliderContentList = document.querySelectorAll('.intro__slider-item__content');
+sliderList.forEach((item) => {
+  item.addEventListener('click', () => {
+    hideSliderContent([...sliderList].indexOf(item));  
+  });
+});
+
+function hideSliderContent(activePos) {
+  sliderContentList.forEach((item) => {
+    const pos = [...sliderContentList].indexOf(item);
+    item.classList.toggle('hidden', pos !== activePos);
+    if (activePos === 0) {
+      sliderList.forEach((item) => {
+        item.style.transform = 'translateX(0)';
+      });
+    } else if (activePos === 1) {
+      sliderList[1].style.transform = 'translateX(-280px)';
+      sliderList[2].style.transform = 'translateX(0)';
+    } else if (activePos === 2) {
+      sliderList[1].style.transform = 'translateX(-280px)';
+      sliderList[2].style.transform = 'translateX(-280px)';
+    }
+  });
+};
