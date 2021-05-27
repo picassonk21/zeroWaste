ca4a18d0177713452253fd6f371fe5e851b72234
Egor Borisevich <picassonk21@gmail.com>
2021-05-27 10:02:13 +0300

    feat: add main page

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const aboutMenuItemList = document.querySelectorAll('.about__menu-item');
+const aboutImage = document.querySelector('.about__image img');
+
+aboutMenuItemList.forEach((item) => {
+  item.addEventListener('click', () => {
+    aboutMenuItemList.forEach((item) => {
+      item.classList.remove('active');
+    });
+    item.classList.add('active');
+    const pos = item.getAttribute('data-pos');
+    aboutImage.src = `./assets/about${pos}.jpg`;
+  });
+});
